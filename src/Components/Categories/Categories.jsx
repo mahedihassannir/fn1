379db890420466979568bdf0090f538488d400cc
sabbir0070 <mdsharifulalamsiddique79672@gmail.com>
2023-08-14 23:32:17 +0600

    odified

---
+++ src/Components/Categories/Categories.jsx
@@ -0,0 +1,57 @@
+
+const Categories = () => {
+  return (
+    <div className='grid grid-cols-8 md:grid-cols-8 lg:grid-cols-8 my-10 gap-2'>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/vBzftrV/img3.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/PgdpJzh/img2.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/vBzftrV/img3.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/PgdpJzh/img2.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/PgdpJzh/img2.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/vBzftrV/img3.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/PgdpJzh/img2.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/vBzftrV/img3.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/vBzftrV/img3.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/vBzftrV/img3.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/vBzftrV/img3.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/vBzftrV/img3.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/vBzftrV/img3.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/vBzftrV/img3.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/vBzftrV/img3.jpg" alt="" />
+      </div>
+      <div className='w-[100%] h-40 border-2  border-gray-500 hover:border-gray-700 hover:shadow-md shadow-slate-900'>
+        <img className='w-full h-full' src="https://i.ibb.co/vBzftrV/img3.jpg" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
