57ebfbf305a9c0a3041c504d7cb2f82939e17db0
unknown <nabilasoma@gmail.com>
2023-08-15 21:50:32 +0600

    all file done

---
+++ src/Components/CodeWorms/CodeWorms.jsx
@@ -0,0 +1,18 @@
+import FlashSale from "../FlashSale/FlashSale";
+import SmallCategory from "../SmallCategory/SmallCategory";
+
+
+const CodeWorms = () => {
+    return (
+        <div>
+           <section> 
+        <SmallCategory></SmallCategory>
+            </section> 
+            <section>
+                <FlashSale></FlashSale>
+            </section>
+        </div>
+    );
+};
+
+export default CodeWorms;
