37ad0f4710ba69e7b06638b62fc6e445d571ad6f
mahedi6666655555 <mahedinir34678@gmail.com>
2023-08-16 22:13:31 +0600

    rear problem

--- src/Components/SmallCategory/SmallCategory.jsx
+++ src/Components/SmallCategory/SmallCategory.jsx
@@ -3,31 +3,31 @@
 const SmallCategory = () => {
     return (
         <div className="mx-20">
-            <div className="mt-12 mx-auto max-w-7xl container mb-2">
+            <div className="mt-12 mx-auto max-w-7xl container ">
                 <h2 className="text-3xl font-bold mb-4">Categories</h2>
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 lg:grid-cols-8 md:gap-x-2">
-            <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+            <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66]">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
             </div>
@@ -36,28 +36,28 @@ const SmallCategory = () => {
 
         <div className="mt-8 mx-auto max-w-7xl container">
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 lg:grid-cols-8">
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                     <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
-                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] mb-2">
+                <div className="w-[150px] bg-slate-300 border-2 border-[#FC9E66] ">
                 <img src="https://i.ibb.co/MgPYXk0/img4.jpg" alt="" />
                 </div>
             </div>
