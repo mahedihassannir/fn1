d1c488db4a243c410a5619688eeabd831091fb0b
mahedi6666655555 <mahedinir34678@gmail.com>
2023-08-17 14:58:05 +0600

    login routes and contex is reay to work with login register

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -1,11 +1,13 @@
 import {
-    createBrowserRouter,
+	createBrowserRouter,
 } from "react-router-dom";
 
 import Main from "../Layout/Layout";
 import Home from "../Pages/Home/Home";
 import Contact from "../Pages/Contact/Contact";
 import SingleProduct from "../Pages/SingleProduct/SingleProduct";
+import Register from "../Login/regester";
+import Login from "../Login/login";
 
 
 const router = createBrowserRouter([
@@ -21,11 +23,25 @@ const router = createBrowserRouter([
 				path: "contact",
 				element: <Contact></Contact>,
 			},
-            {
-                // avengers 
+			{
+				// avengers 
 				path: "products/:id",
 				element: <SingleProduct></SingleProduct>,
 			},
+
+			{
+				// Team Arrow
+				path: "login",
+				element: <Login></Login>,
+			},
+
+			{
+				// Team Arrow 
+				path: "register",
+				element: <Register></Register>,
+			},
+
+
 		],
 	},
 ]);
