f17485d81cd93f7223285e3dd8830966c485610d
unknown <24abdullahalmasum@gmail.com>
2023-08-19 20:56:48 +0600

    completed food section

--- src/Components/fantasticprople/Fashion.jsx
+++ src/Components/fantasticprople/Fashion.jsx
@@ -1,9 +1,21 @@
+// Date:  8/19/23
+// Author:MASUM
+// Description:Display All Fashion Products
+
+import useProducts from "../../Hooks/Fantastic/useProducts";
+
 
 const Fashion = () => {
+    // All Products From Hooks
+    const { products,loading} = useProducts()
+
+    // Filter Only Fashion Category
+    const allFashionProducts = products.filter(FashionProducts=>FashionProducts.category2==="fashion")
+    console.log(allFashionProducts)
     return (
-        <div>
-            
-        </div>
+        <section>
+            <img src="https://i.ibb.co/fYkQS5K/Screenshot-2023-08-17-182335-1.png" alt="" />
+        </section>
     );
 };
 
