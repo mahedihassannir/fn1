3dca01e0e623b3b607ce4a801fdc3101c1237d52
Aniqa <aanika.chowdhury04@gmail.com>
2023-08-19 23:04:35 +0600

    fixed issues

--- src/Components/fantasticprople/Below.jsx
+++ src/Components/fantasticprople/Below.jsx
@@ -40,8 +40,8 @@ const Below = () => {
                     <img src={`https://gotoshop.com.bd/image/cache/catalog/multivendor/4/kidstoys/20201003_004613-500x500.jpg`} />
                     <h1 className=" font-semibold">Product Name:</h1>
                     <div>
-                        <p className=" font-bold">Price</p>
-                        <p className=" text-sm line-through">100</p>
+                        <p className=" font-bold"> Price</p>
+                        <p className=" text-sm line-through"> 100</p>
                     </div>
                     <button className=" text-green-700 font-bold uppercase rounded bg-gray-200 py-1 hover:bg-gray-100">Add To Cart</button>
                 </div>
