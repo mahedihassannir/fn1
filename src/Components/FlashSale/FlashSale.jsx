8a497b66113a90b94f163700e3851be98dea97f0
mahedi6666655555 <mahedinir34678@gmail.com>
2023-08-20 23:20:04 +0600

    done

--- src/Components/FlashSale/FlashSale.jsx
+++ src/Components/FlashSale/FlashSale.jsx
@@ -2,215 +2,77 @@
 // Date:18/8/2023
 // Description:This par is for 'just for you'
 
+import { useEffect, useState } from 'react';
 import { FaStar } from 'react-icons/fa';
 
 
 const FlashSale = () => {
+
+    const [data, SetData] = useState([])
+
+    useEffect(() => {
+
+
+        fetch("http://localhost:5000/allcategory")
+
+            .then(res => res.json())
+
+            .then(data => {
+
+                SetData(data)
+
+            })
+    }, [])
+
+
     return (
-      <div className='mx-16'>
-          <div className="max-w-7xl container sm:w-full">
-            <div>
-                <div className="mt-8 p-4">
-                    <h2 className='text-3xl mb-2 font-bold'>Just For You</h2>
-                    <div className='grid md:mx-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 sm:gap-4 mb-4'>
-                        <div className='w-[230px] hover:shadow-2xl'>
-                            <img src="https://i.ibb.co/PgdpJzh/img2.jpg" alt="bag" />
-                            <div className='p-4 bg-white'>
-                                <p className='font-bold'>New Trendy Look Very Stylish<br /> Black Bag Pack....</p>
-                                <p className='text-[#FC9E66] text-lg font-bold'>TK424</p>
-                                <p><del>Tk900</del> -73%</p>
-                                <div className='flex gap-2'>
-                                    <div className='flex justify-center items-center'>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                    </div>
-                                    <p>(11)</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="w-[230px] hover:shadow-2xl">
-                            <img src="https://i.ibb.co/PgdpJzh/img2.jpg" alt="bag" />
-                            <div className='p-4 bg-white'>
-                                <p className='font-bold'>New Trendy Look Very Stylish<br /> Black Bag Pack....</p>
-                                <p className='text-[#FC9E66] text-lg font-bold'>TK424</p>
-                                <p><del>Tk900</del> -73%</p>
-                                <div className='flex gap-2'>
-                                    <div className='flex justify-center items-center'>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                    </div>
-                                    <p>(11)</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='w-[230px] hover:shadow-2xl'>
-                            <img src="https://i.ibb.co/PgdpJzh/img2.jpg" alt="bag" />
-                            <div className='p-4 bg-white'>
-                                <p className='font-bold'>New Trendy Look Very Stylish<br /> Black Bag Pack....</p>
-                                <p className='text-[#FC9E66] text-lg font-bold'>TK424</p>
-                                <p><del>Tk900</del> -73%</p>
-                                <div className='flex gap-2'>
-                                    <div className='flex justify-center items-center'>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                    </div>
-                                    <p>(11)</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='w-[230px] hover:shadow-2xl'>
-                            <img src="https://i.ibb.co/PgdpJzh/img2.jpg" alt="bag" />
-                            <div className='p-4 bg-white'>
-                                <p className='font-bold'>New Trendy Look Very Stylish<br /> Black Bag Pack....</p>
-                                <p className='text-[#FC9E66] text-lg font-bold'>TK424</p>
-                                <p><del>Tk900</del> -73%</p>
-                                <div className='flex gap-2'>
-                                    <div className='flex justify-center items-center'>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                    </div>
-                                    <p>(11)</p>
-                                </div>
-                            </div>
-                        </div>
+        <div className='lg-11/12 mx-auto   my-20'>
+            <section className="">
+                {/* main grid container */}
+                <div className=" grid md:mx-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 ">
 
-                        <div className='w-[230px] hover:shadow-2xl'>
-                            <img src="https://i.ibb.co/PgdpJzh/img2.jpg" alt="bag" />
-                            <div className='p-4 bg-white'>
-                                <p className='font-bold'>New Trendy Look Very Stylish<br /> Black Bag Pack....</p>
-                                <p className='text-[#FC9E66] text-lg font-bold'>TK424</p>
-                                <p><del>Tk900</del> -73%</p>
-                                <div className='flex gap-2'>
-                                    <div className='flex justify-center items-center'>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
+                    {
+                        data.splice(6, 10).map(res => <div>
+                            {/* this is main card  div starts */}
+                            <div className='lg:w-64 lg:h-96 w-56 h-96  bg-white hover:shadow-md hover:border-2 hover:duration-300 cursor-pointer'>
+                                <img className='w-full h-56' src={res.image} alt="bag" />
+                                <div className='p-4 bg-white'>
+                                    {/* name */}
+                                    <p className='font-bold pb-2'>{res.name}</p>
+                                    {/* name ends */}
+
+                                    {/* price sec */}
+                                    <p className='text-[#FC9E66] text-lg font-bold'> {res.price}</p>
+                                    {/* price sec ends */}
+
+                                    <p><del>Tk900</del> -73%</p>
+                                    <div className='flex gap-2'>
+                                        <div className='flex justify-center items-center'>
+                                            <span className='text-yellow-600'><FaStar></FaStar></span>
+                                            <span className='text-yellow-600'><FaStar></FaStar></span>
+                                            <span className='text-yellow-600'><FaStar></FaStar></span>
+                                            <span className='text-yellow-600'><FaStar></FaStar></span>
+                                            <span className='text-yellow-600'><FaStar></FaStar></span>
+                                        </div>
+                                        <p>(11)</p>
                                     </div>
-                                    <p>(11)</p>
                                 </div>
                             </div>
-                        </div>
-                    </div>
+                        </div>)
+                    }
 
 
 
 
-                    <div className='grid grid-cols-2 md:mx-2 md:grid-cols-3 lg:grid-cols-5 gap-4 sm:gap-4 md:space-x-2'>
-                        <div className='w-[230px] hover:shadow-2xl'>
-                            <img src="https://i.ibb.co/PgdpJzh/img2.jpg" alt="bag" />
-                            <div className='p-4 bg-white'>
-                                <p className='font-bold'>New Trendy Look Very Stylish<br /> Black Bag Pack....</p>
-                                <p className='text-[#FC9E66] text-lg font-bold'>TK424</p>
-                                <p><del>Tk900</del> -73%</p>
-                                <div className='flex gap-2'>
-                                    <div className='flex justify-center items-center'>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                    </div>
-                                    <p>(11)</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="w-[230px] hover:shadow-2xl">
-                            <img src="https://i.ibb.co/PgdpJzh/img2.jpg" alt="bag" />
-                            <div className='p-4 bg-white'>
-                                <p className='font-bold'>New Trendy Look Very Stylish<br /> Black Bag Pack....</p>
-                                <p className='text-[#FC9E66] text-lg font-bold'>TK424</p>
-                                <p><del>Tk900</del> -73%</p>
-                                <div className='flex gap-2'>
-                                    <div className='flex justify-center items-center'>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                    </div>
-                                    <p>(11)</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='w-[230px] hover:shadow-2xl'>
-                            <img src="https://i.ibb.co/PgdpJzh/img2.jpg" alt="bag" />
-                            <div className='p-4 bg-white'>
-                                <p className='font-bold'>New Trendy Look Very Stylish<br /> Black Bag Pack....</p>
-                                <p className='text-[#FC9E66] text-lg font-bold'>TK424</p>
-                                <p><del>Tk900</del> -73%</p>
-                                <div className='flex gap-2'>
-                                    <div className='flex justify-center items-center'>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                    </div>
-                                    <p>(11)</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='w-[230px] hover:shadow-2xl'>
-                            <img src="https://i.ibb.co/PgdpJzh/img2.jpg" alt="bag" />
-                            <div className='p-4 bg-white'>
-                                <p className='font-bold'>New Trendy Look Very Stylish<br /> Black Bag Pack....</p>
-                                <p className='text-[#FC9E66] text-lg font-bold'>TK424</p>
-                                <p><del>Tk900</del> -73%</p>
-                                <div className='flex gap-2'>
-                                    <div className='flex justify-center items-center'>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                    </div>
-                                    <p>(11)</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='w-[230px] hover:shadow-2xl'>
-                            <img src="https://i.ibb.co/PgdpJzh/img2.jpg" alt="bag" />
-                            <div className='p-4 bg-white'>
-                                <p className='font-bold'>New Trendy Look Very Stylish<br /> Black Bag Pack....</p>
-                                <p className='text-[#FC9E66] text-lg font-bold'>TK424</p>
-                                <p><del>Tk900</del> -73%</p>
-                                <div className='flex gap-2'>
-                                    <div className='flex justify-center items-center'>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                        <span className='text-yellow-600'><FaStar></FaStar></span>
-                                    </div>
-                                    <p>(11)</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
-               
-                <div className='mx-auto text-center mb-12 mt-4'>
+
+            </section>
+            {/* this is the  flesh sell card section ends */}
+            <div className='mx-auto text-center mb-12 mt-6'>
                 <button className='py-3 px-32 rounded-lg bg-[#FC9E66] text-white font-bold text-lg text-center'>Load More</button>
-                </div>
-                <div></div>
-                <div></div>
             </div>
+
         </div>
-      </div>
     );
 };
 
