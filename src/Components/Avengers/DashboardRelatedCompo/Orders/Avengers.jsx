bd240b64723a918612901ff5e521dcef82eb4b28
toma570 <itextoma@gmail.com>
2023-08-22 03:10:49 +0600

    add order details

--- src/Components/Avengers/DashboardRelatedCompo/Orders/Avengers.jsx
+++ src/Components/Avengers/DashboardRelatedCompo/Orders/Avengers.jsx
@@ -1,22 +1,16 @@
 
 
+import PageTitleAndRefresh from "../../PageTitleAndRefresh/PageTitleAndRefresh";
+import OrdersDetails from "./OrdersDetails/OrdersDetails";
 const Avengers = () => {
     return (
-		<div className='text-[#0A1727]'>
-			<div className='bg-white p-3 rounded flex items-center justify-between '>
-				<h3 className='text-2xl font-bold'>Orders</h3>
-
-				<div className='flex items-center text-[11px] font-bold gap-2'>
-					<div>
-                        <p>Data Referesh</p>
-                        
-					</div>
-
-					<div className='px-8 border border-[#0a172783] py-1 rounded'>
-						<p>August 22, 2023 01:20 AM</p>
-					</div>
-				</div>
-			</div>
+        <div className='text-[#0A1727]'>
+            
+            <PageTitleAndRefresh title={'Orders'} />
+            
+            <div className="my-4">
+                <OrdersDetails />
+            </div>
 		</div>
 	);
 };
---
+++ src/Components/Avengers/DashboardRelatedCompo/Orders/OrdersDetails/OrdersDetails.jsx
@@ -0,0 +1,213 @@
+/** @format */
+
+import { useState } from "react";
+import { AiOutlineCaretDown } from "react-icons/ai";
+import { IoCloudDoneSharp } from "react-icons/io5";
+import { HiOutlineDotsVertical } from "react-icons/hi";
+const OrdersDetails = () => {
+    const [showCategory, setShowCategory] = useState(false);
+    const [showSorting, setShowSorting] = useState(false);
+	return (
+		<div>
+			<p>Sales period:</p>
+
+			{/* sorting  */}
+			<div className='mt-1 flex justify-between items-center'>
+				<div className='py-1 px-3 border bg-white rounded'>Date</div>
+
+				{/* sort by category and name or price  */}
+				<div className='flex justify-between items-center gap-5'>
+					{/* sort by category */}
+					<div
+						onClick={() => setShowCategory(!showCategory)}
+						className=' border py-1 px-3 bg-white rounded hover:border hover:border-blue-500 duration-500 cursor-pointer w-[180px] flex items-center justify-between relative'
+					>
+						<small>All Products</small>
+
+						<small>
+							<AiOutlineCaretDown
+								className={`${
+									showCategory && "rotate-180 "
+								} duration-300`}
+							/>
+						</small>
+
+						<div
+							className={`px-3  bg-white shadow absolute w-full left-0 top-[115%] overflow-hidden rounded duration-500 ${
+								!showCategory
+									? "max-h-0 opacity-0"
+									: "max-h-[200px] opacity-100 py-1"
+							}`}
+						>
+							<div className='py-[4px]  text-[10px] font-semibold '>
+								All Products
+							</div>
+							<div className='py-[4px]  text-[10px] font-semibold '>
+								Electronics
+							</div>
+							<div className='py-[4px]  text-[10px] font-semibold '>
+								Groceries
+							</div>
+							<div className='py-[4px]  text-[10px] font-semibold '>
+								Fashion
+							</div>
+							<div className='py-[4px]  text-[10px] font-semibold '>
+								Services
+							</div>
+						</div>
+					</div>
+
+					{/* sort by name or price  */}
+					<div
+						onClick={() => setShowSorting(!showSorting)}
+						className=' border py-1 px-3 bg-white rounded hover:border hover:border-blue-500 duration-500 cursor-pointer w-[180px] flex items-center justify-between relative'
+					>
+						<small>All Products</small>
+
+						<small>
+							<AiOutlineCaretDown
+								className={`${
+									showSorting && "rotate-180 "
+								} duration-300`}
+							/>
+						</small>
+
+						<div
+							className={`px-3  bg-white shadow absolute w-full left-0 top-[115%] overflow-hidden rounded duration-500 ${
+								!showSorting
+									? "max-h-0 opacity-0"
+									: "max-h-[200px] opacity-100  py-1"
+							}`}
+						>
+							<div className='py-[4px] text-[10px] font-semibold '>
+								Default Sorting
+							</div>
+							<div className='py-[4px]  text-[10px] font-semibold '>
+								By Name: A-Z
+							</div>
+							<div className='py-[4px]  text-[10px] font-semibold '>
+								By Name: Z-A
+							</div>
+							<div className='py-[4px]  text-[10px] font-semibold '>
+								Ratting: High to Low
+							</div>
+							<div className='py-[4px]  text-[10px] font-semibold '>
+								Ratting: Low to High
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/* order details  */}
+			<div className='mt-4  grid grid-cols-5 gap-5 '>
+				{/* order completed */}
+				<div className='px-3 py-4 col-span-1  bg-white shadow-[0px_0px_10px_rgba(0,0,0,.2)] flex flex-col gap-2'>
+					<div className='flex items-center justify-between'>
+						<div className='w-[30px] h-[30px] rounded-sm bg-[#035ECF] flex items-center justify-center text-xl text-white'>
+							<IoCloudDoneSharp />
+						</div>
+
+						<div>
+							<HiOutlineDotsVertical className='cursor-pointer text-xl text-[#035ecf]' />
+						</div>
+					</div>
+					{/* title  */}
+					<small>
+						<p className='font-semibold'>Orders Completed</p>
+					</small>
+
+					<div>
+						<p className='text-2xl font-bold leading-4'>2345</p>
+					</div>
+				</div>
+
+				{/* order confirmed */}
+				<div className='px-3 py-4 col-span-1  bg-white shadow-[0px_0px_10px_rgba(0,0,0,.2)] flex flex-col gap-2'>
+					<div className='flex items-center justify-between'>
+						<div className='w-[30px] h-[30px] rounded-sm bg-[#00BA9D] flex items-center justify-center text-xl text-white'>
+							<IoCloudDoneSharp />
+						</div>
+
+						<div>
+							<HiOutlineDotsVertical className='cursor-pointer text-xl text-[#035ecf]' />
+						</div>
+					</div>
+					{/* title  */}
+					<small>
+						<p className='font-semibold'>Orders Completed</p>
+					</small>
+
+					<div>
+						<p className='text-2xl font-bold leading-4'>2345</p>
+					</div>
+				</div>
+
+				{/* order canceled  */}
+				<div className='px-3 py-4 col-span-1  bg-white shadow-[0px_0px_10px_rgba(0,0,0,.2)] flex flex-col gap-2'>
+					<div className='flex items-center justify-between'>
+						<div className='w-[30px] h-[30px] rounded-sm bg-[#FF5470] flex items-center justify-center text-xl text-white'>
+							<IoCloudDoneSharp />
+						</div>
+
+						<div>
+							<HiOutlineDotsVertical className='cursor-pointer text-xl text-[#035ecf]' />
+						</div>
+					</div>
+					{/* title  */}
+					<small>
+						<p className='font-semibold'>Orders Completed</p>
+					</small>
+
+					<div>
+						<p className='text-2xl font-bold leading-4'>2345</p>
+					</div>
+				</div>
+
+				{/* order canceled  */}
+				<div className='px-3 py-4 col-span-1  bg-white shadow-[0px_0px_10px_rgba(0,0,0,.2)] flex flex-col gap-2'>
+					<div className='flex items-center justify-between'>
+						<div className='w-[30px] h-[30px] rounded-sm bg-[#035ECF] flex items-center justify-center text-xl text-white'>
+							<IoCloudDoneSharp />
+						</div>
+
+						<div>
+							<HiOutlineDotsVertical className='cursor-pointer text-xl text-[#035ecf]' />
+						</div>
+					</div>
+					{/* title  */}
+					<small>
+						<p className='font-semibold'>Orders Completed</p>
+					</small>
+
+					<div>
+						<p className='text-2xl font-bold leading-4'>2345</p>
+					</div>
+				</div>
+
+				{/* order canceled  */}
+				<div className='px-3 py-4 col-span-1  bg-white shadow-[0px_0px_10px_rgba(0,0,0,.2)] flex flex-col gap-2'>
+					<div className='flex items-center justify-between'>
+						<div className='w-[30px] h-[30px] rounded-sm bg-[#035ECF] flex items-center justify-center text-xl text-white'>
+							<IoCloudDoneSharp />
+						</div>
+
+						<div>
+							<HiOutlineDotsVertical className='cursor-pointer text-xl text-[#035ecf]' />
+						</div>
+					</div>
+					{/* title  */}
+					<small>
+						<p className='font-semibold'>Orders Completed</p>
+					</small>
+
+					<div>
+						<p className='text-2xl font-bold leading-4'>2345</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default OrdersDetails;
