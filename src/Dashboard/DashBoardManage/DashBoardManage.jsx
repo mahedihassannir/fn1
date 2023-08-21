6ee298aff53b59833d00f0684561eba1b8e8f7d7
toma2023 <mrinmoyehimadrita@gmail.com>
2023-08-21 12:13:06 +0600

    sidebar added

--- src/Dashboard/DashBoardManage/DashBoardManage.jsx
+++ src/Dashboard/DashBoardManage/DashBoardManage.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "../adminpanel/Sidebar/Sidebar";
+import Sidebar from "../Avengers/adminpanel/Sidebar/Sidebar";
+
 
 const DashBoardManage = () => {
     return (
-        <div>
+        <div className="flex">
 
+            <div className="w-[20%] h-[100%] bg-[#0A1727]">
             <Sidebar></Sidebar>
+            </div>
             {/* outlet */}
+            <div className="flex-1 ">
             <Outlet></Outlet>
+            </div>
 
         </div>
     );
