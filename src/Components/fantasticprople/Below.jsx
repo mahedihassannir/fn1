e836c7457847975370a2024b8f2211a9ad28a383
Aniqa <aanika.chowdhury04@gmail.com>
2023-08-21 22:37:46 +0600

    added dynamic data for kids and low price route

--- src/Components/fantasticprople/Below.jsx
+++ src/Components/fantasticprople/Below.jsx
@@ -1,5 +1,6 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import useProducts from '../../Hooks/Fantastic/useProducts';
 
 const Below = () => {
     {/*
@@ -13,6 +14,9 @@ const Below = () => {
             ok
             
             */}
+    const { products } = useProducts()
+    const below499 = products?.filter(x => x?.category === "below")
+
     return (
         <div className="m-10">
             <Swiper
@@ -36,15 +40,18 @@ const Below = () => {
 
             </Swiper>
             <div className="grid grid-cols-6 gap-5 my-10">
-                <div className=" grid gap-5 p-5 hover:shadow">
-                    <img src={`https://gotoshop.com.bd/image/cache/catalog/multivendor/4/kidstoys/20201003_004613-500x500.jpg`} />
-                    <h1 className=" font-semibold">Product Name:</h1>
-                    <div>
-                        <p className=" font-bold"> Price</p>
-                        <p className=" text-sm line-through"> 100</p>
-                    </div>
-                    <button className=" text-green-700 font-bold uppercase rounded bg-gray-200 py-1 hover:bg-gray-100">Add To Cart</button>
-                </div>
+                {
+                    below499?.map((x, index) =>
+                        <div key={index} className=" grid gap-5 p-5 hover:shadow">
+                            <img src={x.image} />
+                            <h1 className=" font-semibold">{x.name}</h1>
+                            <div>
+                                <p className=" font-bold">{x.price}</p>
+                                <p className=" text-sm line-through">100</p>
+                            </div>
+                            <button className=" text-green-700 font-bold uppercase rounded bg-gray-200 py-1 hover:bg-gray-100">Add To Cart</button>
+                        </div>)
+                }
             </div>
         </div>
     );
--- src/Components/fantasticprople/Kids.jsx
+++ src/Components/fantasticprople/Kids.jsx
@@ -1,7 +1,14 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import useProducts from '../../Hooks/Fantastic/useProducts';
+
+
 
 const Kids = () => {
+    const { products } = useProducts()
+    const kids = products?.filter(x => x?.category === "kids")
+
+    console.log(kids);
     return (
         <div className="m-10">
             <Swiper
@@ -18,22 +25,26 @@ const Kids = () => {
                 modules={[Autoplay, Pagination, Navigation]}
                 className="mySwiper"
             >
-                <SwiperSlide><img src="https://i.ibb.co/LdDVZKz/Untitled-2.jpg"/></SwiperSlide>
+                <SwiperSlide><img src="https://i.ibb.co/LdDVZKz/Untitled-2.jpg" /></SwiperSlide>
                 <SwiperSlide><img src="https://i.ibb.co/LdDVZKz/Untitled-2.jpg" /></SwiperSlide>
                 <SwiperSlide><img src="https://i.ibb.co/LdDVZKz/Untitled-2.jpg" /></SwiperSlide>
                 <SwiperSlide><img src="https://i.ibb.co/LdDVZKz/Untitled-2.jpg" /></SwiperSlide>
 
             </Swiper>
+
             <div className="grid grid-cols-6 gap-5 my-10">
-                <div className=" grid gap-5 p-5 hover:shadow">
-                    <img src={`https://gotoshop.com.bd/image/cache/catalog/multivendor/4/kidstoys/20201003_004613-500x500.jpg`} />
-                    <h1 className=" font-semibold">Toy Name:</h1>
-                    <div>
-                        <p className=" font-bold">Price</p>
-                        <p className=" text-sm line-through">100</p>
-                    </div>
-                    <button className=" text-green-700 font-bold uppercase rounded bg-gray-200 py-1 hover:bg-gray-100">Add To Cart</button>
-                </div>
+                {
+                    kids?.map((x,index )=>
+                        <div key={index} className=" grid gap-5 p-5 hover:shadow">
+                            <img src={x.image} />
+                            <h1 className=" font-semibold">{x.name}</h1>
+                            <div>
+                                <p className=" font-bold">{x.price}</p>
+                                <p className=" text-sm line-through">100</p>
+                            </div>
+                            <button className=" text-green-700 font-bold uppercase rounded bg-gray-200 py-1 hover:bg-gray-100">Add To Cart</button>
+                        </div>)
+                }
             </div>
         </div>
     );
