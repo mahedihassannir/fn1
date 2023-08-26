1a5946ac75b61a383abe4401923be59004512f99
Aniqa <aanika.chowdhury04@gmail.com>
2023-08-26 19:28:43 +0600

    updated wishlist

--- src/Dashboard/FantasticUserPanel/UserWhichlist/UserWhichlist.jsx
+++ src/Dashboard/FantasticUserPanel/UserWhichlist/UserWhichlist.jsx
@@ -3,7 +3,7 @@ import { RxCross1 } from 'react-icons/rx';
 const UserWhichlist = () => {
     return (
         <div>
-            <h1 className="text-center uppercase font-bold py-3 text-green-600">Wishlist</h1>
+            <h1 className="text-center uppercase text-gray-600 text-xl font-semibold">Wishlist</h1>
             <div>
                 <table className="mx-auto my-20 w-full border-b-2">
                     <thead>
@@ -16,26 +16,34 @@ const UserWhichlist = () => {
                     </thead>
                     <tbody>
                         <tr className=" grid grid-cols-5 bg-white">
-                            <td className=' text-xl'><RxCross1/></td>
+                            <td className=' text-xl text-red-700'><RxCross1/></td>
                             <td><img src="https://cdn.britannica.com/51/190751-050-147B93F7/soccer-ball-goal.jpg" alt="" className=" w-20" /></td>
                             <td>Product Name</td>
                             <td>1000 Taka</td>
-                            <td className=" underline">Add to Cart</td>
+                            <td className=" underline text-green-600">Add to Cart</td>
                         </tr>
                         <tr className=" grid grid-cols-5 bg-white">
-                            <td className=' text-xl'><RxCross1/></td>
+                            <td className=' text-xl text-red-700'><RxCross1/></td>
                             <td><img src="https://cdn.britannica.com/51/190751-050-147B93F7/soccer-ball-goal.jpg" alt="" className=" w-20" /></td>
                             <td>Product Name</td>
                             <td>1000 Taka</td>
-                            <td className=" underline">Add to Cart</td>
+                            <td className=" underline text-green-600">Add to Cart</td>
                         </tr>
                         <tr className=" grid grid-cols-5 bg-white">
-                            <td className=' text-xl'><RxCross1/></td>
+                            <td className=' text-xl text-red-700'><RxCross1/></td>
                             <td><img src="https://cdn.britannica.com/51/190751-050-147B93F7/soccer-ball-goal.jpg" alt="" className=" w-20" /></td>
                             <td>Product Name</td>
                             <td>1000 Taka</td>
-                            <td className=" underline">Add to Cart</td>
+                            <td className=" underline text-green-600">Add to Cart</td>
                         </tr>
+                        <tr className=" grid grid-cols-5 bg-white">
+                            <td className=' text-xl text-red-700'><RxCross1/></td>
+                            <td><img src="https://cdn.britannica.com/51/190751-050-147B93F7/soccer-ball-goal.jpg" alt="" className=" w-20" /></td>
+                            <td>Product Name</td>
+                            <td>1000 Taka</td>
+                            <td className=" underline text-green-600">Add to Cart</td>
+                        </tr>
+                        
                     </tbody>
                 </table>
             </div>
