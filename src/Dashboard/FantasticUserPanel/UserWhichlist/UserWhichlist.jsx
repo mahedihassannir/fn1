b6f9e40b791f3c4638960fc5280c41e4bb62fb18
Aniqa <aanika.chowdhury04@gmail.com>
2023-08-26 19:25:16 +0600

    updated wishlist

--- src/Dashboard/FantasticUserPanel/UserWhichlist/UserWhichlist.jsx
+++ src/Dashboard/FantasticUserPanel/UserWhichlist/UserWhichlist.jsx
@@ -1,42 +1,40 @@
+import { RxCross1 } from 'react-icons/rx';
 
 const UserWhichlist = () => {
     return (
         <div>
-            <h1 className="text-center uppercase font-bold">Wishlist</h1>
+            <h1 className="text-center uppercase font-bold py-3 text-green-600">Wishlist</h1>
             <div>
-                <table className="">
+                <table className="mx-auto my-20 w-full border-b-2">
                     <thead>
-                        <tr>
-                            <th>No.</th>
+                        <tr className="grid grid-cols-5 text-gray-500">
+                            <th></th>
                             <th>Product Name</th>
                             <th>Product Image</th>
                             <th>Price</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>No.</td>
-                            <td>Product Name</td>
+                        <tr className=" grid grid-cols-5 bg-white">
+                            <td className=' text-xl'><RxCross1/></td>
                             <td><img src="https://cdn.britannica.com/51/190751-050-147B93F7/soccer-ball-goal.jpg" alt="" className=" w-20" /></td>
-                            <td>Price</td>
+                            <td>Product Name</td>
+                            <td>1000 Taka</td>
+                            <td className=" underline">Add to Cart</td>
                         </tr>
-                        <tr>
-                            <td>No.</td>
-                            <td>Product Name</td>
+                        <tr className=" grid grid-cols-5 bg-white">
+                            <td className=' text-xl'><RxCross1/></td>
                             <td><img src="https://cdn.britannica.com/51/190751-050-147B93F7/soccer-ball-goal.jpg" alt="" className=" w-20" /></td>
-                            <td>Price</td>
+                            <td>Product Name</td>
+                            <td>1000 Taka</td>
+                            <td className=" underline">Add to Cart</td>
                         </tr>
-                        <tr>
-                            <td>No.</td>
-                            <td>Product Name</td>
+                        <tr className=" grid grid-cols-5 bg-white">
+                            <td className=' text-xl'><RxCross1/></td>
                             <td><img src="https://cdn.britannica.com/51/190751-050-147B93F7/soccer-ball-goal.jpg" alt="" className=" w-20" /></td>
-                            <td>Price</td>
-                        </tr>
-                        <tr>
-                            <td>No.</td>
                             <td>Product Name</td>
-                            <td><img src="https://cdn.britannica.com/51/190751-050-147B93F7/soccer-ball-goal.jpg" alt="" className=" w-20" /></td>
-                            <td>Price</td>
+                            <td>1000 Taka</td>
+                            <td className=" underline">Add to Cart</td>
                         </tr>
                     </tbody>
                 </table>
