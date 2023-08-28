acb67d5757bd2edfd31c3762ac50d9c1f6e029e2
Aniqa <aanika.chowdhury04@gmail.com>
2023-08-28 17:46:23 +0600

    latest code

--- src/Dashboard/FantasticUserPanel/UserpaymentHistory/UserpaymentHistory.jsx
+++ src/Dashboard/FantasticUserPanel/UserpaymentHistory/UserpaymentHistory.jsx
@@ -1,44 +1,49 @@
-import { useEffect, useState } from "react";
+import useOrders from "../../../Hooks/Fantastic/useOrders";
+
 
 const UserpaymentHistory = () => {
-   
-        return (
-            <div className="mx-auto">
-                <h1 className="text-center uppercase font-semibold">Payment History</h1>
-                <div className="grid gap-2 my-10">
-                    <div className="grid grid-cols-3 p-5 bg-white shadow text-gray-500 text-sm font-semibold">
-                        <div>
-                            <h1>Order <span>22-08-2023</span></h1>
-                            <p className="text-gray-900">Shop Name</p>
-                            <p className=" underline text-blue-700">View Order Details</p>
-                        </div>
-                        <div>
-                            <h1>Payment Method</h1>
-                            <p>Bkash</p>
-                        </div>
-                        <div className="flex justify-end items-center">
-                            <p><span className=" text-red-800 text-xl">-</span> 2000 Taka</p>
-                        </div>
+    const paymentHistory = useOrders()
+    
+    const myHistory=paymentHistory.filter(x.email==='aanika.chowdhury04@gmail.com')
+    console.log(myHistory);
+
+    return (
+        <div className="mx-auto">
+            <h1 className="text-center uppercase font-semibold">Payment History</h1>
+            <div className="grid gap-2 my-10">
+                <div className="grid grid-cols-3 p-5 bg-white shadow text-gray-500 text-sm font-semibold">
+                    <div>
+                        <h1>Order <span>22-08-2023</span></h1>
+                        <p className="text-gray-900">Shop Name</p>
+                        <p className=" underline text-blue-700">View Order Details</p>
                     </div>
-                    <div className="grid grid-cols-3 p-5 bg-white shadow text-gray-500 text-sm font-semibold">
-                        <div>
-                            <h1>Order <span>22-08-2023</span></h1>
-                            <p className="text-gray-900">Shop Name</p>
-                            <p className=" underline text-blue-700">View Order Details</p>
-                        </div>
-                        <div>
-                            <h1>Payment Method</h1>
-                            <p>Bkash</p>
-                        </div>
-                        <div className="flex justify-end items-center">
-                            <p><span className=" text-red-800 text-xl">-</span> 2000 Taka</p>
-                        </div>
+                    <div>
+                        <h1>Payment Method</h1>
+                        <p>Bkash</p>
                     </div>
-                    
+                    <div className="flex justify-end items-center">
+                        <p><span className=" text-red-800 text-xl">-</span> 2000 Taka</p>
+                    </div>
+                </div>
+                <div className="grid grid-cols-3 p-5 bg-white shadow text-gray-500 text-sm font-semibold">
+                    <div>
+                        <h1>Order <span>22-08-2023</span></h1>
+                        <p className="text-gray-900">Shop Name</p>
+                        <p className=" underline text-blue-700">View Order Details</p>
+                    </div>
+                    <div>
+                        <h1>Payment Method</h1>
+                        <p>Bkash</p>
+                    </div>
+                    <div className="flex justify-end items-center">
+                        <p><span className=" text-red-800 text-xl">-</span> 2000 Taka</p>
+                    </div>
                 </div>
 
             </div>
-        );
-    };
 
-    export default UserpaymentHistory;
+        </div>
+    );
+};
+
+export default UserpaymentHistory;
